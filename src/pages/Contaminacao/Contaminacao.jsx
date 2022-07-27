cb7cb4a701147fc727a22402f0d6842dc35774ad
tamicristina <tami.cristina06@gmail.com>
2022-07-26 21:05:13 -0300

    criando página sobre contaminacao cruzada

--- src/pages/Contaminacao/Contaminacao.jsx
+++ src/pages/Contaminacao/Contaminacao.jsx
@@ -1,7 +1,36 @@
-function Contaminacao(){
-  return(
-    <h1>Contaminação Cruzada</h1>
-  )
+import "./contaminacao.css";
+
+function Contaminacao() {
+  return (
+    <section className="contaminacao_section">
+      <div className="contaminacao_div_container">
+        <div className="contaminacao_div_text">
+          <h1>Entendendo melhor sobre a Contaminação Cruzada</h1>
+          <p>
+            A contaminação cruzada é a presença de partículas de glúten em
+            alimentos ou utensílios que originalmente não deveriam ter. Essa
+            contaminação pode acontecer durante o plantio, colheita,
+            armazenamento ou preparo dos alimentos. Muitos produtos rotulados
+            com “Não contem glúten” apresentam quantidades mínimas em sua
+            composição e não podem ser consumidos por celíacos. Por exemplo, um
+            produto sem glúten embalado na mesma máquina que se embala produtos
+            com glúten. A contaminação também é frequente em silos de
+            armazenagem e transporte de alimentos, por isso todas as vezes que
+            descobrir um produto novo isento de glúten o celíaco deve entrar em
+            contato com o SAC para verificar se é seguro para o consumo. Outro
+            exemplo clássico é o reaproveitamento de óleo para frituras, em um
+            mesmo recipiente frita-se pastel e batatas, certamente partículas de
+            glúten vão aderir as batatas causando danos ao celíaco.
+          </p>
+        </div>
+        <div className="contaminacao_div_video">
+          
+        
+        
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Contaminacao
+export default Contaminacao;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,24 +10,67 @@ function Home() {
           <div className="section_text">
             <h1>Boas Vindas</h1>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Cupiditate totam dicta quasi, possimus maiores illo tempore
-              incidunt porro blanditiis ducimus sunt nihil! Nisi atque odit
-              temporibus, minus quas quidem vero. Lorem ipsum dolor sit, amet
-              consectetur adipisicing elit. Cupiditate totam dicta quasi,
-              possimus maiores illo tempore incidunt porro blanditiis ducimus
-              sunt nihil! Nisi atque odit temporibus, minus quas quidem vero.
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Cupiditate totam dicta quasi, possimus maiores illo tempore
-              incidunt porro blanditiis ducimus sunt nihil! Nisi atque odit
-              temporibus, minus quas quidem vero.
+              <br />
+              Olá, seja bem vinde ao Hello Celíaco, meu nome é
+              <span className="green_text"> Tamires Cristina</span>, tenho 29
+              anos e sou de Diadema - SP. <br/>Se você está por aqui provavelmente
+              teve o diagnóstico recente da doença celíaca, ou está aprendendo
+              mais sobre, e o objetivo desse site é acolher quem teve o
+              diagnóstico da doença e passar algumas orientações iniciais que
+              possam ajudar no período de adaptação.
+            </p>
+            <h2>Quando eu me descobri celíaca?</h2>
+            <p>
+              Tive o diagnóstico da doença em{" "}
+              <span className="green_text">03/2020</span> e a partir desse
+              momento minha vida mudou completamente ! Confesso que no começo eu
+              fiquei apavorada, não sabia nem o que era glúten mas sabia que
+              minha vida ficaria completamente restrita por conta desse
+              diagnóstico.
+            </p>
+            <h2>Adaptação</h2>
+            <p>
+              Os primeiros meses foram bem difíceis pois eu tive que abrir mão
+              dos meus alimentos preferidos e sabia que nunca mais poderia
+              come-los, mas a situação só foi piorando quando eu descobri sobre
+              a contaminação cruzada, e vamos falar melhor sobre ela mais pra
+              frente.
+            </p>
+            <h2>Há males que vem para o bem...</h2>
+            <p>
+              Conforme eu fui deixando de consumir o glúten, eu fui sentindo
+              melhora em diversos sintomas que eu sempre tive e achava que
+              estava relacionado a outras doenças, um exemplo, eu sempre tive
+              uma anemia chata a vida toda que nunca consegui curar, e foi eu
+              parar de consumir glúten e adivinhem? A anemia foi embora, e fora
+              diversos sintomas gástricos que eu achava que estava relacionado a
+              gastrite e era tudo culpa do bendito do glúten
+            </p>
+            <h2>Vida Social</h2>
+            <p>
+              Confesso que essa foi a parte mais difícil, primeiro porque a
+              maioria das pessoas não sabem o que é glúten, e muito menos que
+              existe uma doença séria relacionada a ele. Por diversas vezes eu
+              ouvi as pessoas falando “Ah, mas é frescura”, “Na minha época não
+              existia isso”, “Você é fresca, não pode comer isso ou aquilo”. E
+              foram poucas as pessoas que se interessaram em saber mais a
+              respeito da doença e tentar entende-la pra tentar fazer eu me
+              sentir melhor de alguma forma…
+              <br /> <br />O conselho que eu deixo pra vocês é, não se deixem
+              abalar por pessoas ignorantes que não se dão nem ao trabalho de
+              pesquisar e entender sobre a doença antes de criticar. Hoje eu
+              ainda sofro com o fato de não poder sair e comer livremente em
+              qualquer lugar, e ainda estou encontrando formas de me adaptar com
+              isso, mas de uma coisa eu falo com toda a certeza, estou bem mais
+              saudável do que a alguns anos atrás e tirar o glúten da minha vida
+              foi a melhor coisa que eu fiz.
             </p>
           </div>
         </div>
         <div className="section_slider_container">
+          <h3>Entendendo melhor sobre o assunto</h3>
           <Slider />
         </div>
-      
       </section>
     </>
   );
