f2a43c89fdb14c14676be3d986c4cd663163de09
tamicristina <tami.cristina06@gmail.com>
2022-07-29 21:44:43 -0300

    ajustando imagens

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,8 @@ import './header.css'
 function Header (){
   return (
     <header className="header">
-      <img className="img_header"src="../../../src/assets/helloceliaco_logo.png"/>
-      <img className="img_header_two"src="../../../src/assets/teste.svg"/>
+      <img className="img_header" src="https://i.ibb.co/hgjKkB7/helloceliaco-logo.png"/>
+      <img className="img_header_two"src="https://i.ibb.co/8BTD8BF/undraw-workout-gcgu.png"/>
 
     </header>
   )
