5db888707d65fda109c262d7d9f71cd45a9e54f1
tamicristina <tami.cristina06@gmail.com>
2022-08-06 13:30:16 -0300

    ajustando imagem do header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,20 @@
-import './header.css'
+import "./header.css";
 
-function Header (){
+function Header() {
   return (
     <header className="header">
-      <img className="img_header" src="https://i.ibb.co/hgjKkB7/helloceliaco-logo.png"/>
-      <img className="img_header_two"src="https://i.ibb.co/8BTD8BF/undraw-workout-gcgu.png"/>
-
+      <div className="header_container">
+      <img
+        className="img_header"
+        src="https://i.ibb.co/F0QQYzR/Logomarca-confeitaria-3.png"
+      />
+      <img
+        className="img_header_two"
+        src="https://i.ibb.co/8BTD8BF/undraw-workout-gcgu.png"
+      />
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
